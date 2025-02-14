c00b10cf4195c08375c5d9f8399505c0a9fc843c
Thijs Lemmens <thijs.lemmens@xenit.eu>
2025-02-14 13:54:50 +0100

    ACC-1786 Testability improvements

--- test/src/test/java/com/contentgrid/platform/runtime/helm/HelmIntegrationTest.java
+++ test/src/test/java/com/contentgrid/platform/runtime/helm/HelmIntegrationTest.java
@@ -2,6 +2,8 @@ package com.contentgrid.platform.runtime.helm;
 
 import com.contentgrid.helm.Helm;
 import com.contentgrid.helm.HelmInstallCommand.InstallOption;
+import com.contentgrid.junit.jupiter.docker.registry.DockerRegistryCache;
+import com.contentgrid.junit.jupiter.externalsecrets.ClusterSecretStore;
 import com.contentgrid.junit.jupiter.externalsecrets.FakeSecretStore;
 import com.contentgrid.junit.jupiter.helm.HelmClient;
 import com.contentgrid.junit.jupiter.k8s.K8sTestUtils;
@@ -19,6 +21,10 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Slf4j
+@DockerRegistryCache(name = "docker.io", proxy = "https://registry-1.docker.io")
+@DockerRegistryCache(name = "quay.io", proxy = "https://quay.io")
+@DockerRegistryCache(name = "ghcr.io", proxy = "https://ghcr.io")
+@DockerRegistryCache(name = "docker.contentgrid.com", proxy = "https://docker.contentgrid.com")
 @Testcontainers
 @KubernetesTestCluster(providers = K3sCiliumDefaultDenyCoreDNSClusterProvider.class)
 @HelmClient
@@ -35,8 +41,14 @@ class HelmIntegrationTest {
 
     static KubernetesClient client;
 
+    static ClusterSecretStore fakeClusterSecretStore;
+
     @BeforeAll
     static void beforeAll() {
+        fakeClusterSecretStore.addSecrets(Map.of(
+                "keycloak.db.password", pgKeycloak.getPassword(),
+                "surveyor.pegman.systems.secret", "{\"hello\": \"world\"}"));
+
         var chart = Path.of("../contentgrid-rtp-helm").toAbsolutePath().normalize();
 
         log.info("Build chart dependencies {}", chart);
@@ -47,16 +59,23 @@ class HelmIntegrationTest {
         log.info("Install chart {}", chart);
         var releaseName = "test";
 
-        var result = helm.install().chart(releaseName, chart,
+        helm.install().chart(releaseName, chart,
                 InstallOption.values(Map.of(
+                        "secretStoreName", fakeClusterSecretStore.getName(),
+                        "keycloak.db.secretKey", "keycloak.db.password",
+                        "surveyor.pegman.systems.secretKey", "surveyor.pegman.systems.secret",
                         "keycloakx.database.hostname", pgKeycloak.getHost(),
                         "keycloakx.database.port", pgKeycloak.getFirstMappedPort(),
                         "keycloak.host", "auth.contentgrid.test",
                         "surveyor.pegman.host", "metrics.contentgrid.test",
-                        "tokenmonger.host", "extensions.contentgrid.test"
+                        "tokenmonger.host", "extensions.contentgrid.test",
+                        "solon.image", "ghcr.io/xenit-eu/contentgrid-solon:0.0.1-SNAPSHOT"
                 )));
 
-        K8sTestUtils.waitUntilDeploymentsReady(10*60, List.of("gateway"), client);
+        K8sTestUtils.waitUntilDeploymentsReady(10 * 60,
+                List.of("gateway", "liaison", "navigator", "pathfinder", "pathfinder-for-webapp",
+                        "slingshot", "surveyor-cgapp-api-exporter", "surveyor-pegman",
+                        "surveyor-postgres-exporter", "solon" ,"tokenmonger"), client);
     }
 
 
