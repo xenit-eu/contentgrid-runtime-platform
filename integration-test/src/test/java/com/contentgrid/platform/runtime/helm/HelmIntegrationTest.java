41f4cf4c00757ea57ff366eca421055364aced06
Lars Vierbergen <lars.vierbergen@xenit.eu>
2025-10-16 16:33:23 +0200

    [ACC-2328] Parameterize deployment manifests

--- integration-test/src/test/java/com/contentgrid/platform/runtime/helm/HelmIntegrationTest.java
+++ integration-test/src/test/java/com/contentgrid/platform/runtime/helm/HelmIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.contentgrid.platform.runtime.helm;
 
+import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -15,37 +16,41 @@ import com.contentgrid.junit.jupiter.k8s.KubernetesTestCluster;
 import com.contentgrid.junit.jupiter.k8s.providers.K3sTestcontainersClusterProvider;
 import com.contentgrid.junit.jupiter.k8s.wait.KubernetesResourceWaiter;
 import com.contentgrid.junit.jupiter.k8s.wait.ResourceMatcher;
+import com.contentgrid.junit.jupiter.k8s.wait.resource.AwaitableResource;
 import com.contentgrid.platform.runtime.helm.HelmIntegrationTest.CustomClusterProvider;
 import com.contentgrid.testcontainers.k3s.customizer.ClusterDomainsK3sContainerCustomizer;
 import com.contentgrid.testcontainers.k3s.customizer.LoggingK3sContainerCustomizer;
 import com.contentgrid.testcontainers.k3s.customizer.cilium.DefaultDenyCiliumK3sContainerCustomizer;
 import com.contentgrid.testcontainers.k3s.customizer.ingress.TraefikIngressK3sContainerCustomizer;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.fabric8.kubernetes.api.model.NamespaceBuilder;
 import io.fabric8.kubernetes.api.model.SecretBuilder;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.hc.client5.http.SystemDefaultDnsResolver;
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
 import org.apache.hc.client5.http.impl.io.BasicHttpClientConnectionManager;
-import org.apache.hc.client5.http.socket.ConnectionSocketFactory;
-import org.apache.hc.client5.http.socket.PlainConnectionSocketFactory;
-import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.client5.http.ssl.DefaultClientTlsStrategy;
+import org.apache.hc.client5.http.ssl.TlsSocketStrategy;
 import org.apache.hc.core5.http.config.RegistryBuilder;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -168,75 +173,17 @@ class HelmIntegrationTest {
     }
 
 
-    @Test
-    void testDeployApplication() throws UnknownHostException, JsonProcessingException {
-        var k8sAppClientConfig = kubernetesClient.getConfiguration();
-        k8sAppClientConfig.setNamespace(APP_NAMESPACE);
-        var appClient = new KubernetesClientBuilder().withConfig(k8sAppClientConfig).build();
+    @ParameterizedTest
+    @CsvSource({"v1", "v2"})
+    void testDeployApplication(String dockerImageTag) throws IOException {
 
-        var dbSecret = new SecretBuilder()
-                .withNewMetadata()
-                .withName("3e5ad186-1fe3-40d5-8891-404571597e30-db" )
-                .withAnnotations(Map.of(
-                        "api.sp.captain.contentgrid.com/db-access-credentials-id",
-                        "cg-c2ebcae8-a3b2-4f2d-8109-e89aaa920756-293162d7-ee7b-406b-b3d4"
-                ))
-                .withLabels(Map.of(
-                        "app.contentgrid.com/app-id", "3e5ad186-1fe3-40d5-8891-404571597e30",
-                        "app.contentgrid.com/application-id", "3e5ad186-1fe3-40d5-8891-404571597e30",
-                        "app.contentgrid.com/service-type", "api",
-                        "app.kubernetes.io/managed-by", "contentgrid",
-                        "captain.contentgrid.com/resource-id", "1064246e-db41-4023-8368-776c03285962"
-                ))
-                .endMetadata()
-                .withType("Opaque" )
-                .addToStringData("spring.datasource.password", appDatabase.getPassword())
-                .addToStringData("spring.datasource.url", appDatabase.getJdbcUrl())
-                .addToStringData("spring.datasource.username", appDatabase.getUsername())
-                .build();
-        //deploy the secret
-        appClient.secrets().resource(dbSecret).create();
-
-        //deploy src/test/resources/testapp/manifest.yaml
-        var manifestInputStream = HelmIntegrationTest.class.getClassLoader()
-                .getResourceAsStream("testapp/manifests.yaml" );
-        appClient.load(manifestInputStream).serverSideApply();
-
-        new KubernetesResourceWaiter(kubernetesClient)
-                .include(Deployment.class, ResourceMatcher.named("api-d-7631ce24-4843-4661-814a-19fea8f0b470").inNamespace(APP_NAMESPACE))
-                .include(Deployment.class, ResourceMatcher.named("openpolicyagent"))
-                .await(wait -> wait.atMost(2, TimeUnit.MINUTES));
-
-
-        var gwSecret = new SecretBuilder()
-                .withNewMetadata()
-                .withGenerateName("gateway-iam-" )
-                .withAnnotations(Map.of(
-                        "gw.sp.captain.contentgrid.com/confidential-client-id", "db03b46f-2f58-402e-87c5-76849505bb7f"
-                ))
-                .withLabels(Map.of(
-                        "app.contentgrid.com/app-id", "3e5ad186-1fe3-40d5-8891-404571597e30",
-                        "app.contentgrid.com/application-id", "3e5ad186-1fe3-40d5-8891-404571597e30",
-                        "app.contentgrid.com/service-type", "gateway",
-                        "app.kubernetes.io/managed-by", "contentgrid",
-                        "captain.contentgrid.com/resource-id", "b83bc515-65ff-45b9-84cd-05fa002104dd"
-                ))
-                .endMetadata()
-                .withType("Opaque" )
-                .addToStringData("contentgrid.idp.client-id",
-                        "contentgrid-app-gateway-ccaa8db6-2514-4680-a2ad-01de8cab8922" )
-                .addToStringData("contentgrid.idp.client-secret", "7fe30b6e-f104-4bf4-9510-e1165eb12865" )
-                .addToStringData("contentgrid.idp.issuer-uri",
-                        "http://auth.contentgrid.test/realms/cg-fff710df-7947-403a-8f45-a3fa97b9b4b2" )
-                .build();
-
-        appClient.secrets().resource(gwSecret).create();
+        var applicationId = deployApplication("ghcr.io/xenit-eu/contentgrid-rtp-test-app:"+dockerImageTag);
 
         var dockerHostAddress = InetAddress.getByName(DockerClientFactory.instance().dockerHostIpAddress());
 
         var client = getRestClient(Map.of(
                 "auth.contentgrid.test", new InetAddress[]{dockerHostAddress},
-                "3e5ad186-1fe3-40d5-8891-404571597e30.apps.contentgrid.test", new InetAddress[]{dockerHostAddress}
+                applicationId+".apps.contentgrid.test", new InetAddress[]{dockerHostAddress}
         ));
 
         var response = client
@@ -257,7 +204,7 @@ class HelmIntegrationTest {
 
         var suppliersResponse = client
                 .get()
-                .uri("http://3e5ad186-1fe3-40d5-8891-404571597e30.apps.contentgrid.test/suppliers" )
+                .uri("http://"+applicationId+".apps.contentgrid.test/suppliers" )
                 .header("Authorization", "Bearer " + accessToken)
                 .retrieve()
                 .toEntity(String.class);
@@ -268,7 +215,7 @@ class HelmIntegrationTest {
         // Expect an exception due to 403 response. The client has no access to invoices via the policies in the app
         HttpClientErrorException exception = assertThrows(HttpClientErrorException.Forbidden.class, () -> {
             client.get()
-                    .uri("http://3e5ad186-1fe3-40d5-8891-404571597e30.apps.contentgrid.test/invoices")
+                    .uri("http://"+applicationId+".apps.contentgrid.test/invoices")
                     .header("Authorization", "Bearer " + accessToken)
                     .retrieve()
                     .toEntity(String.class); // This line throws the exception
@@ -280,13 +227,7 @@ class HelmIntegrationTest {
     }
 
     static RestClient getRestClient(Map<String, InetAddress[]> hosts) {
-        var connectionManager = new BasicHttpClientConnectionManager(
-                RegistryBuilder.<ConnectionSocketFactory>create()
-                        .register("http", PlainConnectionSocketFactory.getSocketFactory())
-                        .register("https", SSLConnectionSocketFactory.getSocketFactory())
-                        .build(),
-                null,
-                null,
+        var connectionManager = BasicHttpClientConnectionManager.create(null,
                 new SystemDefaultDnsResolver() {
                     @Override
                     public InetAddress[] resolve(final String host) throws UnknownHostException {
@@ -297,7 +238,12 @@ class HelmIntegrationTest {
 
                         return super.resolve(host);
                     }
-                });
+                },
+                RegistryBuilder.<TlsSocketStrategy>create()
+                        .register("https", DefaultClientTlsStrategy.createDefault())
+                        .build(),
+                null
+        );
 
         var httpClient = HttpClientBuilder.create()
                 .setConnectionManager(connectionManager)
@@ -307,6 +253,89 @@ class HelmIntegrationTest {
                 .requestFactory(new HttpComponentsClientHttpRequestFactory(httpClient));
 
         return restClientBuilder.build();
+    }
+
+    private String deployApplication(String dockerImage) throws IOException {
+
+        var applicationId = UUID.randomUUID().toString();
+        var deploymentId = UUID.randomUUID().toString();
+        var policyPackage = "contentgrid.userapps.x"+deploymentId.replace("-", "");
+
+        var k8sAppClientConfig = kubernetesClient.getConfiguration();
+        k8sAppClientConfig.setNamespace(APP_NAMESPACE);
+        var appClient = new KubernetesClientBuilder().withConfig(k8sAppClientConfig).build();
+
+        var dbSecret = new SecretBuilder()
+                .withNewMetadata()
+                .withName(applicationId+"-db" )
+                .withLabels(Map.of(
+                        "app.contentgrid.com/app-id", applicationId,
+                        "app.contentgrid.com/application-id", applicationId,
+                        "app.contentgrid.com/service-type", "api",
+                        "app.kubernetes.io/managed-by", "contentgrid"
+                ))
+                .endMetadata()
+                .withType("Opaque" )
+                .addToStringData("spring.datasource.password", appDatabase.getPassword())
+                .addToStringData("spring.datasource.url", appDatabase.getJdbcUrl())
+                .addToStringData("spring.datasource.username", appDatabase.getUsername())
+                .build();
+        //deploy the secret
+        appClient.secrets().resource(dbSecret).create();
+
+        //deploy src/test/resources/testapp/manifest.yaml
+        var manifestInputStream = HelmIntegrationTest.class.getClassLoader()
+                .getResourceAsStream("testapp/manifests.yaml" );
+        String manifestContent = new String(manifestInputStream.readAllBytes(), StandardCharsets.UTF_8);
+
+        // Replace variables
+        manifestContent = manifestContent
+                .replace("$APP_ID", applicationId)
+                .replace("$DEPLOYMENT_ID", deploymentId)
+                .replace("$POLICY_PACKAGE", policyPackage)
+                .replace("$DOCKER_IMAGE", dockerImage);
+
+        appClient.load(new ByteArrayInputStream(manifestContent.getBytes(StandardCharsets.UTF_8)))
+                .serverSideApply();
+
+        new KubernetesResourceWaiter(kubernetesClient)
+                .include(Deployment.class, ResourceMatcher.named("api-d-"+deploymentId).inNamespace(APP_NAMESPACE))
+                .include(Deployment.class, ResourceMatcher.named("openpolicyagent"))
+                .await(wait -> wait.atMost(2, TimeUnit.MINUTES));
+
+        var solonWaiter = new KubernetesResourceWaiter(kubernetesClient)
+                .include(Deployment.class, ResourceMatcher.named("solon"));
+
+        // Wait until Solon has logged the deployment ID, indicating that it has served the new bundle to OPA
+        await()
+                .atMost(1, TimeUnit.MINUTES)
+                .until(() -> solonWaiter.resources()
+                        .flatMap(AwaitableResource::logs)
+                        .anyMatch(logLine -> logLine.line().contains("+ "+deploymentId))
+                );
+
+
+        var gwSecret = new SecretBuilder()
+                .withNewMetadata()
+                .withGenerateName("gateway-iam-" )
+                .withLabels(Map.of(
+                        "app.contentgrid.com/app-id", applicationId,
+                        "app.contentgrid.com/application-id", applicationId,
+                        "app.contentgrid.com/service-type", "gateway",
+                        "app.kubernetes.io/managed-by", "contentgrid"
+                ))
+                .endMetadata()
+                .withType("Opaque" )
+                .addToStringData("contentgrid.idp.client-id",
+                        "contentgrid-app-gateway-ccaa8db6-2514-4680-a2ad-01de8cab8922" )
+                .addToStringData("contentgrid.idp.client-secret", "7fe30b6e-f104-4bf4-9510-e1165eb12865" )
+                .addToStringData("contentgrid.idp.issuer-uri",
+                        "http://auth.contentgrid.test/realms/cg-fff710df-7947-403a-8f45-a3fa97b9b4b2" )
+                .build();
+
+        appClient.secrets().resource(gwSecret).create();
+
+        return applicationId;
     }
 
 }
